0fe67e170838ba9d49de5b9acaaaed02de4c302c
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-05-03 10:51:06 +0600

    Add Sum_of_2Values.c to find pairs summing to x

---
+++ Nested Loop/Sum_of_2Vaules.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <assert.h>
+#include <errno.h>
+
+
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--){
+  int n;
+  bool flag=false;
+  scanf("%d",&n);
+  int a[n];
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  int x;
+  scanf("%d",&x);
+  for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+      if(a[i]+a[j]==x){
+         printf("%d %d\n",a[i],a[j]);
+         printf("%d %d\n",i,j);
+        break;
+       
+      }
+      
+    }
+    
+  }
+
+}
+    return 0;
+}
