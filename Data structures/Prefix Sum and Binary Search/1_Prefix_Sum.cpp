e69bb2fbb5be0a9c4fffd732b997970a200ef11c
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-06-29 18:55:54 +0600

    Add prefix sum implementation in 1_Prefix_Sum.cpp
    Implement prefix sum calculation for an array.

---
+++ Data structures/Prefix Sum and Binary Search/1_Prefix_Sum.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std; 
+       
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+ 
+    int n;cin>>n;
+
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    vector<int>pre(n);
+
+    pre[0]=a[0];
+
+    for(int i=1;i<n;i++){
+        pre[i]=pre[i-1]+a[i];
+    }
+
+
+    for(int i=0;i<n;i++){
+        cout<<pre[i]<<" ";
+    }
+ 
+return 0;
+}
